3ce48b8c99633810472a18767d65ac449a50b275
fadli131 <fadliaryadinata011@gmail.com>
2023-07-29 16:53:54 +0700

    first commit

---
+++ admin.php
@@ -0,0 +1,101 @@
+<?php 
+    session_start();
+      if(!isset($_SESSION['login_user'])) {
+        header("location: login.php");
+      }else{
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+    <title>PETSHOPQU</title>
+  </head>
+  <body>
+
+  <!-- Jumbotron -->
+      <div class="jumbotron jumbotron-fluid text-center">
+      </div>
+  <!-- Akhir Jumbotron -->
+
+  <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg  bg-dark">
+        <div class="container">
+        <a class="navbar-brand text-white" href="admin.php"><strong>PETSHOPQU</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="admin.php">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="daftar_menu.php">DAFTAR STOCK</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="pesanan.php">PESANAN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mr-4" href="logout.php">LOGOUT</a>
+            </li>
+          </ul>
+        </div>
+       </div> 
+      </nav>
+  <!-- Akhir Navbar -->
+
+  <!-- Menu -->    
+      <div class="container">
+        <div class="judul text-center mt-5">
+          <h3 class="font-weight-bold">PETSHOPQU</h3>
+          <h5>Jl. Kelapa 2 Depok</h5>
+          <br>Buka Jam <strong>10:00 - 20:00</strong></h5>
+        </div>
+
+        <div class="row mb-5 mt-5 ">
+          <div class="col-md-6 d-flex justify-content-end">
+            <div class="card bg-dark text-white border-light">
+              <img src="images/background/cing n jing.jpg" class="card-img" alt="Lihat Daftar Menu" height="200">
+              <div class="card-img-overlay mt-5 text-center">
+               <a href="daftar_menu.php" class="btn btn-primary">LIHAT DAFTAR STOCK</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 d-flex justify-content-start">
+            <div class="card bg-dark text-white border-light">
+              <img src="images/background/adopt me.jpg" class="card-img" alt="Lihat Pesanan" height="200">
+              <div class="card-img-overlay mt-5 text-center">
+               <a href="pesanan.php" class="btn btn-primary">LIHAT PESANAN</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  <!-- Akhir Menu -->
+
+  <!-- Awal Footer -->
+  <!-- Akhir Footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+  </body>
+</html>
+<?php } ?>
